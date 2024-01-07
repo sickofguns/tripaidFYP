c42a04535a231d697fd9945d5c600887d32f6c8d
kohweishan <130195750+kohweishan@users.noreply.github.com>
2024-01-07 16:24:27 +1100

    Add files via upload

---
+++ POI/FoodPage.js
@@ -0,0 +1,301 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+const FoodScreen = () => {
+    const currentLocation = 'Orchard, Singapore';
+    const navigation = useNavigation(); // Initialize the navigation object
+
+    
+    // Assuming posts are fetched and stored in this array
+    const [posts, setPosts] = useState([
+      { id: 1, title: 'Post 1', description: 'Description for post 1' },
+      { id: 2, title: 'Post 2', description: 'Description for post 2' },
+      { id: 3, title: 'Post 3', description: 'Description for post 3' },
+      { id: 4, title: 'Post 4', description: 'Description for post 4' },
+      { id: 5, title: 'Post 5', description: 'Description for post 5' },
+      { id: 6, title: 'Post 6', description: 'Description for post 6' },
+
+      // ... more posts ...
+  ]);
+
+  const [likedPosts, setLikedPosts] = useState(Array(posts.length).fill(false));
+
+  const handleLike = (index) => {
+      const updatedLikes = [...likedPosts];
+      updatedLikes[index] = !updatedLikes[index];
+      setLikedPosts(updatedLikes);
+  };
+
+
+
+    return (
+        <View style={styles.container}>
+          <View style={styles.logoheader}>
+            <View style={styles.header}>
+              <MaterialIcons name="location-on" size={20} color="#FF5733" />
+              <Text style={styles.headerText}> {currentLocation}</Text>
+            </View>
+            <Image source={require('../assets/LOGO.png')} style={styles.logo} />
+          </View>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.midContainer}>
+
+              <Text style={styles.mainText}>Explore Food in SG!</Text>
+
+              {/* Instagram Post Containers */}
+              {posts.map((post, index) => (
+                        <View key={post.id} style={styles.instagramPostContainer}>
+                    <Image source={require('../assets/food.jpg')} style={styles.postImage} />
+                    <View style={styles.postContent}>
+                        <View style={styles.userInfo}>
+                            <Image source={require('../assets/LOL.jpg')} style={styles.userAvatar} />
+                            <View>
+                                <Text style={styles.userName}>Naomi Neo</Text>
+                                <Text style={styles.postTime}>Posted 2 hours ago</Text>
+                                <View style={styles.header}>
+                                  <MaterialIcons name="location-on" size={14} color="#000" />
+                                  <Text style={styles.postLocation}>Sentosa Beach, Singapore</Text>
+                                </View>
+                            </View>
+                        </View>
+                        <Text style={styles.postTitle}>Beautiful Destination</Text>
+                        <Text style={styles.postDescription}>
+                            Stunning view of the beach at sunset. Fine City! Fun at Sentosa Beach.
+                        </Text>
+                        <View style={styles.postInteractions}>
+                            <TouchableOpacity
+                                style={styles.interactionButton}
+                                onPress={() => handleLike(index)} // Pass index of the post to handleLike function
+                            >
+                                <MaterialIcons
+                                    name={likedPosts[index] ? 'favorite' : 'favorite-border'}
+                                    size={24}
+                                    color={likedPosts[index] ? 'red' : '#333'}
+                                />
+                                <Text style={styles.interactionText}>Like</Text>
+                            </TouchableOpacity>
+                            <TouchableOpacity style={styles.interactionButton}>
+                                <MaterialIcons name="mode-comment" size={24} color="#333" />
+                                <Text style={styles.interactionText}>Comment</Text>
+                            </TouchableOpacity>
+                        </View>
+                    </View>
+                </View>
+                ))}
+
+                
+                
+
+
+                
+
+              <View style={styles.footerImages}>
+                <Image source={require('../assets/HILTON.jpg')} style={styles.footerImageHilton} />
+                <View style={styles.spacerfooter} />
+                <Image source={require('../assets/SIA.jpg')} style={styles.footerImageSIA} />
+                <View style={styles.spacerfooter} />
+                <Image source={require('../assets/GBB.jpg')} style={styles.footerImageGBB} />
+              </View>
+              <Text style={{marginBottom: 20}}>&copy; 2023 TripAid</Text>   
+
+            </View>
+            </ScrollView>
+                    
+    <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF',
+      paddingTop: 15,
+    },
+    scrollContainer: {
+      flexGrow: 1,
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      paddingVertical: 20,
+      paddingTop: 20,
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerText: {
+      fontSize: 14,
+      color: '#6A778B',
+    },
+    logoheader: {
+      flexDirection: 'column',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    logo: {
+      width: 50,
+      height: 50,
+      resizeMode: 'contain',
+      marginBottom: 5,
+      marginTop: 2,
+    },
+    footer: {
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      paddingVertical: 20,
+    },
+    footerImages: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+    },
+    footerImageHilton: {
+      width: 53.84,
+      height: 20,
+      resizeMode: 'cover',
+    },
+    footerImageSIA: {
+      width: 195.03,
+      height: 20,
+      resizeMode: 'cover',
+      marginLeft: 17,
+      marginRight: 17,
+    },
+    footerImageGBB: {
+      width: 50,
+      height: 47,
+      resizeMode: 'cover',
+    },
+
+    midContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 5,
+      },
+      footerContainer: {
+        flexDirection: 'row',
+        justifyContent: 'space-evenly',
+        alignItems: 'center',
+        backgroundColor: '#FB7E3C',
+        borderTopLeftRadius: 15,
+        borderTopRightRadius: 15,
+        height: 75,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        right: 0,
+      },
+      footerItem: {
+        alignItems: 'center',
+        marginBottom: 10,
+      },
+      footerText: {
+        color: '#FFF',
+        fontSize: 11,
+      },
+      middleCircle: {
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginBottom: 10,
+      },
+      circle: {
+        backgroundColor: '#FDCA60',
+        width: 50,
+        height: 50,
+        borderRadius: 50, // Increased to make the circle more rounded
+        alignItems: 'center',
+        justifyContent: 'center',
+      },
+      plus: {
+        color: '#093D89',
+        fontSize: 50,
+        fontWeight: 'bold',
+        marginTop: -5,
+      },
+
+      mainText: {
+        color: '#093D89',
+        fontSize: 32,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        textAlign: 'center',
+    },
+
+    instagramPostContainer: {
+      backgroundColor: '#FFF',
+      borderRadius: 10,
+      shadowColor: '#000',
+      shadowOffset: {
+          width: 0,
+          height: 3,
+      },
+      shadowOpacity: 0.27,
+      shadowRadius: 4.65,
+      elevation: 6,
+      marginBottom: 20,
+  },
+  postImage: {
+      width: 'auto',
+      height: 200,
+      borderTopLeftRadius: 10,
+      borderTopRightRadius: 10,
+  },
+  postContent: {
+      padding: 10,
+  },
+  postTitle: {
+      fontSize: 18,
+      fontWeight: 'bold',
+      marginBottom: 5,
+  },
+  postDescription: {
+      fontSize: 14,
+      color: '#555',
+  },
+  userInfo: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+},
+userAvatar: {
+    width: 40,
+    height: 40,
+    borderRadius: 20,
+    marginRight: 10,
+},
+userName: {
+    fontSize: 16,
+    fontWeight: 'bold',
+},
+postTime: {
+    fontSize: 12,
+    color: '#888',
+},
+postLocation: {
+  
+  fontSize: 12,
+  color: '#888',
+},
+postInteractions: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    marginTop: 10,
+},
+interactionButton: {
+    flexDirection: 'row',
+    alignItems: 'center',
+},
+interactionText: {
+    marginLeft: 5,
+    color: '#333',
+},
+    
+});
+
+export default FoodScreen;
---
+++ UnregisteredUser/BusinessRegistration.js
@@ -0,0 +1,324 @@
+import React, { useState } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, TextInput, Text, TouchableOpacity, View, Image, ScrollView } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import { Picker } from '@react-native-picker/picker';
+
+const BORegistrationScreen = () => {
+    const currentLocation = 'Orchard, Singapore';
+    const navigation = useNavigation();
+
+    const [ACRA, setACRA] = useState('');
+    const category = ['Hospitality', 'Food & Beverage', 'Sports & Recreation', 'Retail'];
+    const country = ['Local', 'International'];
+    const hour = [
+        "9 AM - 5 PM", "10 AM - 6 PM", "8:30 AM - 4:30 PM", "11 AM - 7 PM", "9:30 AM - 5:30 PM",
+        "12 PM - 8 PM", "1 PM - 9 PM", "8 AM - 4 PM", "10:30 AM - 6:30 PM", "11:30 AM - 7:30 PM", "24 Hours"
+    ];
+    const hostsize = [
+        "Small (1-20 guests)", "Medium (20-50 guests)", "Large (50-100 guests)", "Extra Large (100+ guests)",
+        "Intimate (1-10 guests)", "Intimate Gathering (1-15 guests)", "Cozy (10-30 guests)",
+        "Spacious (30-70 guests)", "Grand (70-150 guests)", "Massive (150+ guests)"
+    ];
+
+    const [selectedCategory, setSelectedCategory] = useState('Category');
+    const [selectedCountry, setSelectedCountry] = useState('Local or International');
+    const [selectedHour, setSelectedHour] = useState('Operating Hours');
+    const [selectedHostSize, setSelectedHostSize] = useState('No. of Pax');
+
+    const handleContinuePress = () => {
+        console.log('ACRA:', ACRA);
+        console.log('Category:', selectedCategory);
+        console.log('Country:', selectedCountry);
+        console.log('Operating Hours:', selectedHour);
+        console.log('No. of Pax:', selectedHostSize);
+
+        navigation.navigate('Business Registration Category');
+    };
+
+    const handleCancelPress = () => {
+        navigation.navigate('Account Type');
+    };
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+                <MaterialIcons name="location-on" size={20} color="#FF5733" />
+                <Text style={styles.headerText}> {currentLocation}</Text>
+            </View>
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+                <View style={styles.midContainer}>
+                    <Image source={require('../assets/LOGO.png')} style={styles.logo} />
+                    <Text style={styles.enterDOBText}>Enter your ACRA number</Text>
+                    <Text style={styles.additionalText}>Your business information will be protected</Text>
+                    <View style={styles.dropdownContainer}>
+                        <TextInput
+                            style={styles.input}
+                            placeholder="ACRA Number"
+                            onChangeText={(text) => setACRA(text)}
+                            value={ACRA}
+                        />
+                    </View>
+
+                    <Text style={styles.enterDOBText}>Select your Business Category</Text>
+                    <View style={styles.dropdownContainer}>
+                        <Picker
+                            selectedValue={selectedCategory}
+                            onValueChange={(itemValue) => setSelectedCategory(itemValue)}
+                            style={styles.CategorydropdownBox}
+                        >
+                            <Picker.Item label="Business Category" value="Business Category" />
+                            {category.map((category) => (
+                                <Picker.Item
+                                    label={category.toString()}
+                                    value={category.toString()}
+                                    key={category.toString()}
+                                />
+                            ))}
+                        </Picker>
+                    </View>
+
+                    <Text style={styles.enterDOBText}>
+                Business Operating Country</Text>
+
+                <View style={styles.dropdownContainer}>
+                  {/* local/international Picker */}
+                  <Picker
+                    selectedValue={selectedCountry}
+                    onValueChange={(itemValue) => setSelectedCountry(itemValue)}
+                    style={styles.CountrydropdownBox}
+                  >
+                    <Picker.Item label="Local or International" value="Local or International" />
+                    {country.map((country) => (
+                      <Picker.Item label={country.toString()} value={country.toString()} key={country.toString()} />
+                    ))}
+                  </Picker>
+                </View>
+
+                <Text style={styles.enterDOBText}>
+                Operating Hours</Text>
+
+                <View style={styles.dropdownContainer}>
+                  {/* local/international Picker */}
+                  <Picker
+                    selectedValue={selectedHour}
+                    onValueChange={(itemValue) => setSelectedHour(itemValue)}
+                    style={styles.HourdropdownBox}
+                  >
+                    <Picker.Item label="Operating Hours" value="Operating Hours" />
+                    {hour.map((hour) => (
+                      <Picker.Item label={hour.toString()} value={hour.toString()} key={hour.toString()} />
+                    ))}
+                  </Picker>
+                </View>
+
+                <Text style={styles.enterDOBText}>
+                Hosting Size</Text>
+
+                <View style={styles.dropdownContainer}>
+                  {/* local/international Picker */}
+                  <Picker
+                    selectedValue={selectedHostSize}
+                    onValueChange={(itemValue) => setSelectedHostSize(itemValue)}
+                    style={styles.HostsizedropdownBox}
+                  >
+                    <Picker.Item label="No. of Pax" value="No. of Pax" />
+                    {hostsize.map((hostsize) => (
+                      <Picker.Item label={hostsize.toString()} value={hostsize.toString()} key={hostsize.toString()} />
+                    ))}
+                  </Picker>
+                </View>
+                    <TouchableOpacity style={styles.continueButton} onPress={handleContinuePress}>
+                        <Text style={styles.buttonText}>Continue</Text>
+                    </TouchableOpacity>
+
+                    <TouchableOpacity onPress={handleCancelPress}>
+                        <Text style={styles.cancelText}>Cancel</Text>
+                    </TouchableOpacity>
+                </View>
+            </ScrollView>
+
+            <View style={styles.footer}>
+                <View style={styles.footerImages}>
+                    <Image source={require('../assets/HILTON.jpg')} style={styles.footerImageHilton} />
+                    <View style={styles.spacerfooter} />
+                    <Image source={require('../assets/SIA.jpg')} style={styles.footerImageSIA} />
+                    <View style={styles.spacerfooter} />
+                    <Image source={require('../assets/GBB.jpg')} style={styles.footerImageGBB} />
+                </View>
+                <Text>&copy; 2023 TripAid</Text>
+            </View>
+            <StatusBar style="auto" />
+        </View>
+    );
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF',
+      paddingTop: 15,
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerText: {
+      fontSize: 14,
+      color: '#6A778B',
+    },
+    logo: {
+      width: 50,
+      height: 50,
+      resizeMode: 'contain',
+      marginBottom: 60,
+    },
+    footer: {
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      paddingVertical: 20,
+    },
+    footerImages: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+    },
+    footerImageHilton: {
+      width: 53.84,
+      height: 20,
+      resizeMode: 'cover',
+    },
+    footerImageSIA: {
+      width: 195.03,
+      height: 20,
+      resizeMode: 'cover',
+      marginLeft: 17,
+      marginRight: 17,
+    },
+    footerImageGBB: {
+      width: 50,
+      height: 47,
+      resizeMode: 'cover',
+    },
+
+    midContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 10,
+      },
+    enterDOBText: {
+        color: '#FB7E3C',
+        fontSize: 16,
+        fontWeight: 'bold',
+        marginBottom: 40,
+        textAlign: 'center',
+        marginTop: -50,
+    },
+    additionalText: {
+        color: '#847C7C',
+        fontSize: 12,
+        textAlign: 'center',
+        marginHorizontal: 30, // Adjust the margin as needed
+        lineHeight: 17, // Controls line height
+        marginTop: -40,
+    },
+    dropdownContainer: {
+      flexDirection: 'row',
+      justifyContent: 'space-between',
+      marginHorizontal: 40,
+      marginTop: 5,
+      marginBottom: 65,
+    },
+    dropdownText: {
+      color: '#6C6363',
+      fontSize: 16,
+      fontWeight: 'bold',
+    },   
+    continueButton: {
+      backgroundColor: '#030D45',
+      borderRadius: 5,
+      height: 50,
+      width: 320,
+      marginTop: -30,
+      justifyContent: 'center', // Center content vertically
+      alignItems: 'center', // Center content horizontally
+    },
+    buttonText: {
+      color: '#FFF',
+      fontSize: 20,
+      fontWeight: 'bold',
+      textAlign: 'center',
+    },
+    cancelText: {
+      color: '#847C7C',
+      fontSize: 16,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      marginTop: 15,
+    },
+
+    
+    CategorydropdownBox: {
+      width: '100%',
+      height: 51,
+      borderRadius: 6,
+      borderColor: '#6C6363',
+      borderWidth: 1,
+      justifyContent: 'center',
+      marginTop: -30,
+      overflow: 'hidden', // Hide the content that overflows from the container
+    },
+    CountrydropdownBox: {
+        width: '100%',
+        height: 51,
+        borderRadius: 6,
+        borderColor: '#6C6363',
+        borderWidth: 1,
+        justifyContent: 'center',
+        marginTop: -30,
+        overflow: 'hidden', // Hide the content that overflows from the container
+      },
+      HourdropdownBox: {
+        width: '100%',
+        height: 51,
+        borderRadius: 6,
+        borderColor: '#6C6363',
+        borderWidth: 1,
+        justifyContent: 'center',
+        marginTop: -30,
+        overflow: 'hidden', // Hide the content that overflows from the container
+      },
+      HostsizedropdownBox: {
+        width: '100%',
+        height: 51,
+        borderRadius: 6,
+        borderColor: '#6C6363',
+        borderWidth: 1,
+        justifyContent: 'center',
+        marginTop: -30,
+        overflow: 'hidden', // Hide the content that overflows from the container
+      },
+    input: {
+        height: 60,
+        width: 326,
+        marginVertical: 5,
+        paddingHorizontal: 10,
+        backgroundColor: '#FFF',
+        borderRadius: 5,
+        borderWidth: 1, // Sets the width of the outline
+        borderColor: '#C4C4C4',
+        backgroundColor: '#F7F7F7',
+      },
+      scrollContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingBottom: 30, // Adjust the bottom padding as needed
+    },
+});
+
+export default BORegistrationScreen;
