db40cc281f3a301381cafc9e8ea982c69b59e054
kohweishan <130195750+kohweishan@users.noreply.github.com>
2024-01-14 03:28:17 +1100

    Add files via upload

---
+++ UnregisteredUser/FnBBOSubCat.js
@@ -0,0 +1,194 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+const BOSubCategoryFnbScreen = () => {
+    const navigation = useNavigation(); // Initialize the navigation object
+    const handlePress = () => {
+        navigation.navigate('Account Successfully Registered'); // Navigate to SignUp screen on button press
+    };
+
+    return (
+        <View style={styles.container}>
+            
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.midContainer}>
+                
+                <Text style={styles.selectText}>Food & Beverage {'\n'} Sub-Business</Text>
+
+                {/* Search Bar */}
+                <View style={styles.searchBar}>
+                        <MaterialIcons name="search" size={20} color="#757575" style={styles.searchIcon} />
+                        <TextInput
+                            style={styles.searchInput}
+                            placeholder="Search Category"
+                            placeholderTextColor="#757575"
+                        />
+                </View>
+
+                <TouchableOpacity onPress={handlePress}>
+                <View style={styles.curvedContainer}>
+                    <Image
+                    source={require('../assets/Hawker.jpg')}
+                    style={styles.image}
+                    />
+                    <Text style={styles.textBehindImage}>Hawker Centres</Text>
+                </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handlePress}>
+                <View style={styles.curvedContainer}>
+                    <Image
+                    source={require('../assets/Restaurants.jpg')}
+                    style={styles.image}
+                    />
+                    <Text style={styles.textBehindImage}>Restaurants</Text>
+                </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handlePress}>
+                <View style={styles.curvedContainer}>
+                    <Image
+                    source={require('../assets/Cafe.jpg')}
+                    style={styles.image}
+                    />
+                    <Text style={styles.textBehindImage}>Cafe</Text>
+                </View>
+                </TouchableOpacity>
+
+                <TouchableOpacity onPress={handlePress}>
+                <View style={styles.curvedContainer}>
+                    <Image
+                    source={require('../assets/Bar.jpg')}
+                    style={styles.image}
+                    />
+                    <Text style={styles.textBehindImage}>Bars</Text>
+                </View>
+                </TouchableOpacity>
+
+            
+
+                
+            </View>
+        </ScrollView>
+
+            <View style={styles.footer}>
+          <Text>&copy; 2023 TripAid</Text>
+        </View>
+        
+    <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF',
+      paddingTop: 15,
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingVertical: 20,
+        paddingTop: 20,
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerText: {
+      fontSize: 14,
+      color: '#6A778B',
+    },
+    logo: {
+      width: 50,
+      height: 50,
+      resizeMode: 'contain',
+      marginBottom: 20,
+    },
+    footer: {
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      paddingVertical: 20,
+    },
+
+    midContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 10,
+      },
+      selectText: {
+        color: '#0A2753',
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+      },
+      curvedContainer: {
+        width: 300,
+        height: 140,
+        borderRadius: 30,
+        overflow: 'hidden',
+        position: 'relative',
+        marginBottom: 30,
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        opacity: 0.6, // Adjust the opacity level here (0 to 1)
+      },
+      textBehindImage: {
+        position: 'absolute',
+        zIndex: -1,
+        color: 'blue',
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        width: '100%',
+        marginTop: 50, // Adjust the positioning of the text as needed
+      },
+
+      editIconContainer: {
+        position: 'absolute',
+        bottom: 5,
+        right: 5,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        borderRadius: 12,
+        padding: 4,
+        marginRight: 10,
+        marginBottom: 5,
+      },
+      editIcon: {
+        // Style for the edit icon
+      },
+
+      searchBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: '#E2E2E2',
+        borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: '#F7F7F7',
+        paddingHorizontal: 10,
+        marginBottom: 20,
+        width: '90%',
+    },
+    searchIcon: {
+        marginRight: 10,
+    },
+    searchInput: {
+        fontSize: 15,
+        color: '#757575',
+        flex: 1,
+    },    
+});
+
+export default BOSubCategoryFnbScreen;
