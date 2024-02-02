3a38a4bbc7a4b0af134bb2017263f0e4c6663f2d
kohweishan <130195750+kohweishan@users.noreply.github.com>
2024-02-03 02:32:27 +0800

    Add files via upload

---
+++ SystemAdmin/RetailSubCat.js
@@ -0,0 +1,182 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView, TextInput } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+
+
+const SubCategoryRetailScreen = () => {
+    const navigation = useNavigation(); // Initialize the navigation object
+
+    const subcategories = [
+      { id: 1, image: require('../assets/fashions.jpg'), category: 'Fashion' },
+      { id: 2, image: require('../assets/beauty.jpg'), category: 'Beauty' },
+      { id: 3, image: require('../assets/IT.jpg'), category: 'IT' },
+    ];
+
+    const CategoryButtons = () => {
+      const [searchText, setSearchText] = useState('');
+      const filteredCategories = subcategories.filter(category => category.category.toLowerCase().includes(searchText.toLowerCase()));
+    
+    return (
+        <View style={styles.container}>
+            
+            <ScrollView contentContainerStyle={styles.scrollContainer}>
+            <View style={styles.midContainer}>
+                
+                <Text style={styles.selectText}>Retail {'\n'} Sub-Business</Text>
+
+                {/* Search Bar */}
+                <View style={styles.searchBar}>
+                  <MaterialIcons name="search" size={20} color="#757575" style={styles.searchIcon} />
+                  <TextInput
+                    style={styles.searchInput}
+                    placeholder="Search Category"
+                    placeholderTextColor="#757575"
+                    value={searchText}
+                    onChangeText={(text) => setSearchText(text)}
+                  />
+                </View>
+
+                {/* Display filtered subcategories */}
+                {filteredCategories.map((item) => (
+                    <View style={styles.curvedContainer} key={item.id}>
+                      <Image source={item.image} style={styles.image} />
+                      <Text style={styles.textBehindImage}>{item.category}</Text>
+                    </View>
+                ))}
+
+            </View>
+        </ScrollView>
+
+            <View style={styles.footer}>
+                    <TouchableOpacity onPress={navigation.goBack}>
+                        <Text style={styles.backText}>Back</Text>
+                    </TouchableOpacity>
+          <Text>&copy; 2024 TripAid</Text>
+        </View>
+        
+    <StatusBar style="auto" />
+    </View>
+  );
+};
+
+return <CategoryButtons />;
+};
+
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF',
+      paddingTop: 15,
+    },
+    scrollContainer: {
+        flexGrow: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        paddingVertical: 20,
+        paddingTop: 20,
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerText: {
+      fontSize: 14,
+      color: '#6A778B',
+    },
+    logo: {
+      width: 50,
+      height: 50,
+      resizeMode: 'contain',
+      marginBottom: 20,
+    },
+    footer: {
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      paddingVertical: 20,
+    },
+
+    midContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 10,
+      },
+      selectText: {
+        color: '#0A2753',
+        fontSize: 32,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        marginBottom: 20,
+      },
+      curvedContainer: {
+        width: 300,
+        height: 140,
+        borderRadius: 30,
+        overflow: 'hidden',
+        position: 'relative',
+        marginBottom: 30,
+      },
+      image: {
+        width: '100%',
+        height: '100%',
+        resizeMode: 'cover',
+        opacity: 0.6, // Adjust the opacity level here (0 to 1)
+      },
+      textBehindImage: {
+        position: 'absolute',
+        zIndex: -1,
+        color: 'blue',
+        fontSize: 28,
+        fontWeight: 'bold',
+        textAlign: 'center',
+        width: '100%',
+        marginTop: 50, // Adjust the positioning of the text as needed
+      },
+
+      editIconContainer: {
+        position: 'absolute',
+        bottom: 5,
+        right: 5,
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        borderRadius: 12,
+        padding: 4,
+        marginRight: 10,
+        marginBottom: 5,
+      },
+      editIcon: {
+        // Style for the edit icon
+      },
+
+      searchBar: {
+        flexDirection: 'row',
+        alignItems: 'center',
+        borderColor: '#E2E2E2',
+        borderWidth: 1,
+        borderRadius: 5,
+        backgroundColor: '#F7F7F7',
+        paddingHorizontal: 10,
+        marginBottom: 20,
+        width: '90%',
+    },
+    searchIcon: {
+        marginRight: 10,
+    },
+    searchInput: {
+        fontSize: 15,
+        color: '#757575',
+        flex: 1,
+    },   
+    backText: {
+      fontSize: 13,
+      color: '#757575',
+      marginTop: 10,
+      marginBottom: 10,
+      alignSelf: 'center',
+  }, 
+});
+
+export default SubCategoryRetailScreen;
---
+++ UnregisteredUser/RegistrationSuccessPage.js
@@ -0,0 +1,238 @@
+import React, { useState, useEffect } from 'react';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, TouchableOpacity, View, Image, ScrollView, Alert } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+import { MaterialIcons } from '@expo/vector-icons';
+import * as Location from 'expo-location';
+
+
+const RegistrationSuccessScreen = () => {
+  const [currentLocation, setCurrentLocation] = useState('Loading...'); // Initialize with a loading message
+  const [location, setLocation] = useState('');
+
+  useEffect(() => {
+    const fetchLocation = async () => {
+      try {
+        let { status } = await Location.requestForegroundPermissionsAsync();
+        if (status !== 'granted') {
+          throw new Error('Permission to access location was denied');
+        }
+
+        let locationData = await Location.getCurrentPositionAsync({});
+        setLocation(locationData);
+
+        let address = await Location.reverseGeocodeAsync({
+          latitude: locationData.coords.latitude,
+          longitude: locationData.coords.longitude,
+        });
+        
+        // Extract relevant information (street name and country)
+        const street = address[0]?.name || '';
+        const country = address[0]?.country || '';
+        
+        // Set the location state with the obtained information
+        setCurrentLocation(`${street}, ${country}`);
+        
+      } catch (error) {
+        console.error('Error fetching location:', error.message);
+        // Handle the error as needed
+      }
+    };
+
+    fetchLocation();
+  }, []); // Empty dependency array to run the effect only once
+      const navigation = useNavigation(); // Initialize the navigation object
+
+    const handleProceed = () => {
+        navigation.navigate('Log In'); // Navigate to SignUp screen on button press
+    };
+
+    useEffect(() => {
+      // Show an alert when the component mounts
+      Alert.alert(
+        'Success!',
+        'You have successfully registered! An email will be sent for verification.',
+        [{ text: 'OK', onPress: () => {
+          navigation.navigate("Log In")
+        } }] // You can define actions here
+      );
+    }, []); // Empty dependency array to run this effect only once when the component mounts
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.header}>
+              <MaterialIcons name="location-on" size={20} color="#FF5733" />
+              <Text style={styles.headerText}> {currentLocation}</Text>
+            </View>
+            <View style={styles.midContainer}>
+                <Text style={styles.successmsgText}>You are all set!</Text>
+
+                <View style={styles.square}>
+                  <View style={styles.topLine}>
+                    <View style={styles.circle}>
+                      <MaterialIcons
+                        name="flight"
+                        size={55}
+                        color="#FB7E3C"
+                        style={{ transform: [{ rotate: '45deg' }], alignSelf: 'center' }} // Rotate the icon by 45 degrees
+                      />
+                    </View>
+                  </View>
+
+                    <Text style={styles.secondaryText}> 
+                      <Text style={{ color: '#030D45' }}>Ready to explore</Text>
+                      {'\n'}
+                      <Text style={{ color: '#FB7E3C' }}>Singapore together</Text>
+                      <Text style={{ color: '#030D45' }}>?</Text>
+                    </Text>
+                      <Image source={require('../assets/LOGO.png')} style={styles.logo} />
+
+
+                </View>
+
+
+                <TouchableOpacity style={styles.proceedButton} onPress={handleProceed}>
+                    <Text style={styles.proceedButtonText}>Proceed</Text>
+                </TouchableOpacity>       
+
+                
+
+                
+            </View>
+
+            <View style={styles.footer}>
+          <View style={styles.footerImages}>
+            <Image source={require('../assets/HILTON.jpg')} style={styles.footerImageHilton} />
+            <View style={styles.spacerfooter} />
+            <Image source={require('../assets/SIA.jpg')} style={styles.footerImageSIA} />
+            <View style={styles.spacerfooter} />
+            <Image source={require('../assets/GBB.jpg')} style={styles.footerImageGBB} />
+          </View>
+          <Text>&copy; 2024 TripAid</Text>
+        </View>
+        
+    <StatusBar style="auto" />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+    container: {
+      flex: 1,
+      backgroundColor: '#FFF',
+      paddingTop: 15,
+    },
+    header: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    headerText: {
+      fontSize: 14,
+      color: '#6A778B',
+    },
+    logo: {
+      width: 150,
+      height: 150,
+      resizeMode: 'contain',
+      marginTop: 10,
+      alignSelf: 'center', // Center the logo horizontally
+  },
+    footer: {
+      alignItems: 'center',
+      backgroundColor: '#ffffff',
+      paddingVertical: 20,
+    },
+    footerImages: {
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingHorizontal: 20,
+    },
+    footerImageHilton: {
+      width: 53.84,
+      height: 20,
+      resizeMode: 'cover',
+    },
+    footerImageSIA: {
+      width: 195.03,
+      height: 20,
+      resizeMode: 'cover',
+      marginLeft: 17,
+      marginRight: 17,
+    },
+    footerImageGBB: {
+      width: 50,
+      height: 47,
+      resizeMode: 'cover',
+    },
+
+    midContainer: {
+        flex: 1,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+        marginTop: 60,
+      },
+
+    successmsgText: {
+        color: '#FB7E3C',
+        fontSize: 28,
+        fontWeight: 'bold',
+        marginBottom: 30,
+        textAlign: 'center',
+    },
+
+    secondaryText:{
+        color: '#4B4B4B',
+        fontSize: 32,
+        marginBottom: 20,
+        marginTop: 20,
+        textAlign: 'center',
+        fontWeight: 'bold',
+    },
+
+    proceedButton: {
+        backgroundColor: '#030D45',
+        borderRadius: 5,
+        height: 50,
+        width: 320,
+        marginTop: 20,
+        justifyContent: 'center', // Center content vertically
+        alignItems: 'center', // Center content horizontally
+      },
+      proceedButtonText: {
+        color: '#FFF',
+        fontSize: 18,
+        fontWeight: 'bold',
+        textAlign: 'center',
+      },
+      square: {
+        width: 356,
+        height: 374,
+        borderRadius: 16,
+        borderWidth: 2,
+        borderColor: '#FB7E3C',
+        alignItems: 'center', // Center the content horizontally in the square
+        justifyContent: 'center', // Center the content vertically in the square
+        marginBottom: 10, // Add margin bottom for spacing
+        marginTop: 30,
+    },
+    topLine: {
+        flexDirection: 'row',
+        justifyContent: 'center',
+        position: 'absolute', // Position this relative to the parent
+        top: -40, // Adjust this value to position the top line
+        zIndex: 1, // Bring this above other content
+    },
+    circle: {
+        width: 90,
+        height: 90,
+        borderRadius: 80,
+        borderColor: '#FB7E3C',
+        borderWidth: 2,
+        backgroundColor: 'white',
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+});
+
+export default RegistrationSuccessScreen;
